9bcbe880d1ce7d2e56e70b52357c4b94a08285ef
157290182@qq.com <157290182@qq.com>
2022-08-29 11:20:54 +0800

    update new contents

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://java-env.us-east-1.elasticbeanstalk.com/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
